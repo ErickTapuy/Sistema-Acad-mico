0b1444b40b3b2f1a2a2e9c895c7555303bde2ac8
Erick Tapuy <denistapuy17@gmail.com>
2025-07-06 18:01:39 -0500

    Estudiante

---
+++ src/main/java/com/mycompany/sistemaacademico/Estudiante/Estudiante.java
@@ -0,0 +1,34 @@
+
+package com.mycompany.sistemaacademico.Estudiante;
+
+import com.mycompany.sistemaacademico.PersonaInstitucional;
+
+
+public class Estudiante extends PersonaInstitucional{
+    
+    
+    //atributos
+    private int codigo;
+    
+    
+    //    
+
+    public Estudiante(int codigo, String correoInstitucional, int id, String nombre, String correoPersonal, String cedula) {
+        super(correoInstitucional, id, nombre, correoPersonal, cedula);
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "codigo=" + codigo + '}';
+    }
+    
+}
---
+++ src/main/java/com/mycompany/sistemaacademico/Estudiante/Estudiantes.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.sistemaacademico.Estudiante;
+
+
+public class Estudiantes {
+    
+    public String agregarEstudiante(){
+        return "";
+    }
+}

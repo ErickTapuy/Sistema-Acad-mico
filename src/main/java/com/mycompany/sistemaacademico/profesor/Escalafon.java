a6ecdc7a44de583c040f3606918890aa35e3a18e
SanmiguelAyleen <ayleen.dessiresanmiguel@gmail.com>
2025-07-07 16:12:20 -0500

    Profesor

---
+++ src/main/java/com/mycompany/sistemaacademico/profesor/Escalafon.java
@@ -0,0 +1,7 @@
+package com.mycompany.sistemaacademico.profesor;
+
+public enum Escalafon {
+    PRINCIPAL,
+    AGREGADO,
+    AUXILIAR
+}
---
+++ src/main/java/com/mycompany/sistemaacademico/profesor/IProfesores.java
@@ -0,0 +1,8 @@
+package com.mycompany.sistemaacademico.profesor;
+
+public interface IProfesores {
+    
+    public String agregarProfesor();
+    public boolean eliminarProfesor();
+    public boolean actualizarProfesor();
+}

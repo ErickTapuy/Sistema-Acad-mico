b08f1992363bee1e01a0c6aa3cd8fa2cd1cd8051
SanmiguelAyleen <ayleen.dessiresanmiguel@gmail.com>
2025-07-07 14:45:58 -0500

    Trabajador

---
+++ src/main/java/com/mycompany/sistemaacademico/trabajador/ITrabajadores.java
@@ -0,0 +1,8 @@
+package com.mycompany.sistemaacademico.trabajador;
+
+public interface ITrabajadores {
+
+    public boolean agregarTrabajador();
+    public boolean eliminarTrabajador();
+    public boolean actualizarTrabajador();
+}
---
+++ src/main/java/com/mycompany/sistemaacademico/trabajador/Trabajador.java
@@ -0,0 +1,27 @@
+package com.mycompany.sistemaacademico.trabajador;
+
+import com.mycompany.sistemaacademico.Empleado;
+
+public class Trabajador extends Empleado {
+
+    private String gremio;
+
+    public Trabajador(String gremio, double sueldo, String correoInstitucional, int id, String nombre, String correoPersonal, String cedula) {
+        super(sueldo, correoInstitucional, id, nombre, correoPersonal, cedula);
+        this.gremio = gremio;
+    }
+
+    public String getGremio() {
+        return gremio;
+    }
+
+    public void setGremio(String gremio) {
+        this.gremio = gremio;
+    }
+
+    @Override
+    public String toString() {
+        return "Trabajador{" + super.toString() + "gremio=" + gremio + '}';
+    }
+
+}

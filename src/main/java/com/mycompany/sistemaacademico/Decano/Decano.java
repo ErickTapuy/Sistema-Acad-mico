677e9412dffeff92826bf4c069ede52f7d9b5f2d
Erick Tapuy <denistapuy17@gmail.com>
2025-07-07 14:50:48 -0500

    mi rama

---
+++ src/main/java/com/mycompany/sistemaacademico/Decano/Decano.java
@@ -0,0 +1,34 @@
+
+package com.mycompany.sistemaacademico.Decano;
+
+import com.mycompany.sistemaacademico.PersonalAcademico;
+
+
+public class Decano extends PersonalAcademico{
+    
+    //atributos
+    private String nivel;
+    
+    //
+
+    public Decano(String nivel, String dedicacion, double sueldo, String correoInstitucional, int id, String nombre, String correoPersonal, String cedula) {
+        super(dedicacion, sueldo, correoInstitucional, id, nombre, correoPersonal, cedula);
+        this.nivel = nivel;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Decano{" + "nivel=" + nivel + '}';
+    }
+    
+    
+}
---
+++ src/main/java/com/mycompany/sistemaacademico/Decano/Decanos.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.sistemaacademico.Decano;
+
+
+public class Decanos {
+    
+    public String agregarDecano(){
+        return"";
+    }
+    public boolean verificarCedula(){
+        return true;
+    }
+}
